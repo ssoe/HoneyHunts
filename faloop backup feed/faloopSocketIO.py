2c0928c10128f74d45911436fad01d096591e8a4
SSoe <ssoe@users.noreply.github.com>
2023-12-01 18:29:43 +0100

    Update faloopSocketIO.py
    fixing general idiocy

--- faloop backup feed/faloopSocketIO.py
+++ faloop backup feed/faloopSocketIO.py
@@ -44,8 +44,8 @@ def catch_all(event, data):
     #print("event received: ", event)
     filter_data(data)
     
-    with open('received_events.txt', 'a') as file:
-        file.write(f"{data}\n")
+    #with open('received_events.txt', 'a') as file:
+        #file.write(f"{data}\n")
 
 def connectFaloopSocketio(session_id, jwt_token):
     
@@ -73,79 +73,77 @@ def connectFaloopSocketio(session_id, jwt_token):
 #{'type': 'mob', 'subType': 'report', 'data': {'action': 'spawn', 'mobId': 2962, 'worldId': 42, 'zoneInstance': 0, 'data': {'zoneId': 134, 'zonePoiIds': [27], 'timestamp': '2023-11-28T19:53:54.648Z', 'window': 1}}}
 #dead srank
 #{'type': 'mob', 'subType': 'report', 'data': {'action': 'death', 'mobId': 10618, 'worldId': 33, 'zoneInstance': 3, 'data': {'num': 1, 'startedAt': '2023-11-29T19:27:23.322Z', 'prevStartedAt': '2023-11-23T19:48:46.901Z'}}}
-def filter_data(data):
-    # Ensure the data meets your criteria before sending
-    mobs = huntDic['MobDictionary']
+def filter_data(data):  # sourcery skip: extract-method
     if (data.get('type') == 'mob' and 
         data.get('subType') == 'report' and 
         data.get('data', {}).get('action') == 'spawn' and
         str(data['data']['mobId']) in mobs and
         data['data']['mobId'] not in ss):
-            hunt_id = data['data']['mobId']
             world_id = data['data']['worldId']
+            hunt_id = data['data']['mobId']
+            instance = data['data']['zoneInstance']
             zone_id = data['data']['data']['zoneId']
             pos_id = int(data['data']['data']['zonePoiIds'][0])
-            instance = data['data']['zoneInstance']
-            sendSpawn(data, hunt_id, world_id, zone_id, pos_id, instance)
-            
+            if str(world_id) in worlds:
+                sendSpawn(data, hunt_id, world_id, zone_id, pos_id, instance)
+
     if (data.get('type') == 'mob' and 
         data.get('subType') == 'report' and 
         data.get('data', {}).get('action') == 'death' and
         str(data['data']['mobId']) in mobs and
         data['data']['mobId'] not in ss):
-        
-            hunt_id = data['data']['mobId']
             world_id = data['data']['worldId']
+            hunt_id = data['data']['mobId']
             instance = data['data']['zoneInstance']
-            sendDeath(data, hunt_id, world_id, instance)
+            if str(world_id) in worlds:
+                print(data)
+                sendDeath(data, hunt_id, world_id, instance)
     
-def sendSpawn(faloopWebhook, data, hunt_id, world_id, zone_id, pos_id, instance):
+def sendSpawn(data, hunt_id, world_id, zone_id, pos_id, instance):
     worldName = worlds[str(world_id)]
     mobName = mobs[str(hunt_id)]
     zoneName = zones[str(zone_id)]
     coords = getCoords(pos_id, zone_id)
     timer = int(time.time())
-    
+
+    if str(world_id) in lightWorlds:
+        srank_role = srank_role_id
+    elif str(world_id) in chaosWorlds:
+        srank_role = csrank_role_id
     if coords:
         x, y = [value.strip() for value in coords.split(',')]
         mapurl = f"https://api.ffxivsonar.com/render/map?zoneid={zone_id}&flagx={x}&flagy={y}"
-        if world_id in lightWorlds:
-            srank_role = srank_role_id
-        if world_id in chaosWorlds:
-            srank_role = csrank_role_id  
-        
         message = f"<@&{srank_role}> {mobName[0]}, on world: {worldName[0]}, coords: {coords}, zone: {zoneName[0]}, in instance: {instance}, Timestamp: <t:{timer}:R>, {mapurl}"
         zoneIds[(hunt_id, world_id, instance)] = (zone_id)
         faloopWebhook.send(message)
         faloopWebhook.send(data)
         print("Message sent to Discord successfully.")
 
-def sendDeath(faloopWebhook, data, hunt_id, world_id, instance):
+def sendDeath(data, hunt_id, world_id, instance):
     worldName = worlds[str(world_id)]
     mobName = mobs[str(hunt_id)]
     zone_id = zoneIds[(hunt_id, world_id, instance)]
     
     message = f"Srank {mobName[0]} on {worldName[0]} in instance: {instance} died"
     faloopWebhook.send(message)
-    faloopWebhook.send(data)
+    #faloopWebhook.send(data)
     print("death sent to Discord successfully.")
     deleteMapping(world_id, zone_id, instance)
     print("mapping yeeted")
     del zoneIds[(hunt_id, world_id, instance)]    
     
-def getCoords(pos_id, zone_id):
+def getCoords(pos_id, zone_id):  # sourcery skip: extract-method
     try:
         with sqlite3.connect('faloopdb.db') as conn:
             cursor = conn.cursor()
             query = "SELECT coords FROM zone_positions WHERE posId = ? AND zoneId = ?"
             cursor.execute(query, (pos_id, zone_id))
             result = cursor.fetchone()
-            cursor.close()
-            conn.close()
             return result[0] if result else None
     except sqlite3.Error as e:
         print(f"SQLite error: {e}")
         return None
+
 
 def deleteMapping(world_id, zone_id, instance):
     try:
@@ -153,8 +151,8 @@ def deleteMapping(world_id, zone_id, instance):
             cursor = conn.cursor()
             query = "DELETE FROM mapping WHERE world_id = ? AND zone_id = ? AND instance = ?"
             cursor.execute(query, (world_id, zone_id, instance))
-            cursor.close()
-            conn.close()
+            conn.commit()
+            print("mapping yeeted")
     except sqlite3.Error as e:
         print(f"Database error: {e}")
         return f"Failed to delete entries due to DB error: {e}"
